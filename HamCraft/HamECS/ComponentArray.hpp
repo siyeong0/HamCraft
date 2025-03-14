1687554b4aadfa6a7839d728ba01cad291bfd3aa
SIYEONG-PC\siyeong <sy1211w@gmail.com>
2025-03-14 13:10:46 +0900

    ECS base

--- HamCraft/HamECS/ComponentArray.hpp
+++ HamCraft/HamECS/ComponentArray.hpp
@@ -27,19 +27,9 @@ namespace ham
 		void EntityDestroyed(Entity entity) override;
 
 	private:
-		// The packed array of components (of generic type T),
-		// set to a specified maximum amount, matching the maximum number
-		// of entities allowed to exist simultaneously, so that each entity
-		// has a unique spot.
 		std::array<T, MAX_ENTITIES> mComponentArray;
-
-		// Map from an entity ID to an array index.
 		std::unordered_map<Entity, size_t> mEntityToIndexMap;
-
-		// Map from an array index to an entity ID.
 		std::unordered_map<size_t, Entity> mIndexToEntityMap;
-
-		// Total size of valid entries in the array.
 		size_t mSize;
 	};
 
