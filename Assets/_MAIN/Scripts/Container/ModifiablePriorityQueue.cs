14bca20e6b6299699569103c6e900b85843d6386
SIYEONG-PC\siyeong <sy1211w@gmail.com>
2025-04-20 16:59:11 +0900

    Containers; PriorityQueue

---
+++ Assets/_MAIN/Scripts/Container/ModifiablePriorityQueue.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamCraft.Container
+{
+	public class ModifiablePriorityQueue<T, U> : PriorityQueue<T, U>
+	{
+		public T Peek()
+		{
+			if (Count == 0) throw new System.InvalidOperationException();
+			return list[list.Count - 1].item;
+		}
+
+		public void Insert(int index, T item, U priority)
+		{
+			list.Insert(index, (item, priority));
+		}
+
+		public void RemoveAt(int index)
+		{
+			list.RemoveAt(index);
+		}
+
+		public T Find(Predicate<T> match)
+		{
+			return list.First(x => match(x.item)).item;
+		}
+
+		public int FindIndex(Predicate<T> match)
+		{
+			return list.FindIndex(x => match(x.item));
+		}
+
+		public bool Exist(Predicate<T> match)
+		{
+			return list.Any(x => match(x.item));
+		}
+
+		public T this[int index]
+		{
+			get
+			{
+				if (index < 0 || index >= Count) throw new System.ArgumentOutOfRangeException();
+				return list[index].item;
+			}
+			set
+			{
+				if (index < 0 || index >= Count) throw new System.ArgumentOutOfRangeException();
+				list[index] = (value, list[index].priority);
+			}
+		}
+	}
+}
---
+++ Assets/_MAIN/Scripts/Container/PriorityQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using static UnityEngine.Rendering.DebugUI;
+
+namespace HamCraft.Container
+{
+	public class PriorityQueue<T, U>
+	{
+		protected List<(T item, U priority)> list = new List<(T, U priority)>();	// 오름차순
+
+		public int Count => list.Count;
+
+		public void Enqueue(T item, U priority)
+		{
+			list.Add((item, priority));
+			list = list.OrderBy(x => 
+			{
+				object priorityValue;
+
+				if (x.priority is Delegate del)
+				{
+					var returnValue = del.DynamicInvoke();
+					if (returnValue is IComparable comparable)
+						priorityValue = comparable;
+					else
+						throw new InvalidOperationException("Priority Func must return IComparable type");
+				}
+				else if (x.priority is IComparable directComparable)
+				{
+					priorityValue = directComparable;
+				}
+				else
+				{
+					throw new InvalidOperationException("Priority must be either IComparable or Func returning IComparable");
+				}
+
+				return priorityValue;
+			}).ToList();
+		}
+
+		public T Dequeue()
+		{
+			if (Count == 0) throw new System.InvalidOperationException();
+			T item = list[Count - 1].item;
+			list.RemoveAt(Count - 1);
+			return item;
+		}
+
+		public void Clear()
+		{
+			list.Clear();
+		}
+	}
+}
